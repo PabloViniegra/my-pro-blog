73f1fee8d94fbadbd5a49d50d4a098d5e2d7c1db
PabloViniegra <pablovpmadrid@gmail.com>
2025-08-04 00:27:46 +0200

    fix lint error

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,7 +2,7 @@ import { sql } from '@vercel/postgres'
 import { NextRequest, NextResponse } from 'next/server'
 import type { User } from '@/types'
 
-export async function GET(_: NextRequest) {
+export async function GET() {
   const { rows } = await sql<Partial<User>[]>`
       SELECT id, clerk_id, name, avatar_url, created_at FROM users
       ORDER BY created_at DESC
